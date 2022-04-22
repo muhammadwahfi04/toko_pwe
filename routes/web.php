17e64ad85261cfed063dd703d27d6ad30f224a61
Muhammad Wahfi <muhammadwahfi@gmail.com>
2022-04-22 09:26:38 +0700

    Commit Kedua

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,40 @@ Route::get('/nama-lengkap', function () {
     echo '<h1>Nama Lengkap</h1>';
     echo '<p>Nama : Muhammad Wahfi Udin</p>';
 });
+
+Route::get('/npm', function () {
+    echo '<h1>NPM</h1>';
+    echo '<p>NPM : G1A020007</p>';
+});
+
+Route::get('/kelas', function () {
+    echo '<h1>Kelas</h1>';
+    echo '<p>Kelas : Informatika A</p>';
+});
+
+Route::get('/prodi', function () {
+    echo '<h1>Prodi</h1>';
+    echo '<p>Prodi : Teknik</p>';
+});
+
+Route::get('/jurusan', function () {
+    echo '<h1>Jurusan</h1>';
+    echo '<p>Jurusan : Informatika</p>';
+});
+
+Route::get('/universitas', function () {
+    echo '<h1>Universitas</h1>';
+    echo '<p>Universitas : Universitas Bengkulu</p>';
+});
+
+Route::get('/tanggal-lahir', function () {
+    echo '<h1>Tanggal Lahir</h1>';
+    echo '<p>Tanggal Lahir : 4 April 2002</p>';
+});
+
+Route::get('/alamat', function () {
+    echo '<h1>Alamat</h1>';
+    echo '<p>Alamat : Jalan Basuki Rahmat 1, Rt 8, Rw 2, Sukamerindu, Kota Bengkulu</p>';
+});
+
+
